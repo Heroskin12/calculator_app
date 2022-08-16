0c83a51cd42578717085e780640cfa327bb249e4
Jamie Furlong <jamiefurlong16@gmail.com>
2022-08-16 22:08:35 +0100

    Additional Functionality
    Calculator works as intended for all basic arithmetic operators.
    
    New buttons added for storing previous answer and undo.

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ const clear = document.querySelector('.clear'); // C button.
 const negative = document.querySelector('.negative'); // negation button
 const equal = document.querySelector('.equal'); // equal button
 const bracket = document.querySelector('.bracket'); // bracket button
+const answer = document.querySelector('.ans');
+const undo = document.querySelector('.undo');
 let isBracket = false; // bool to determine the existence of an opening bracket.
+let previousAnswer = NaN;
+let temp;
 
 const displayBar = document.querySelector('.display_bar');
 let result = document.querySelector('.result');
@@ -15,34 +19,99 @@ let input = document.querySelector('.input');
 const calculator = {
     firstOperand: null,
     currentOperator: null,
-    currentOperand: null
+    currentOperand: 0,
+    result: 0
 };
 
 
 arrBtn.forEach(btn => {
     btn.addEventListener('click', function() {
-        if (input.innerText === "0") {
-            if (btn.innerText < "0" || btn.innerText > "9") {
-                return input.innerText = "ERROR";
-            }
+        if (input.innerText === "Enter a number.") {
+                if (btn.innerText < "0" || btn.innerText > "9") {
+                    return input.innerText = "ERROR";
+                }
             input.innerText = btn.innerText;
-            calculator.currentOperand = input.innerText;
+            calculator.currentOperand = btn.innerText;
+            console.log(`currentOperand is: ${calculator.currentOperand}`);
         }
         else if (btn.innerText === "+" || btn.innerText === "-" || btn.innerText === "x" || btn.innerText === "/") {
-            input.innerText = input.innerText + btn.innerText;
-            calculator.currentOperator = btn.innerText;
-            console.log(`Current Operator is ${calculator.currentOperator}`);
+            input.innerText += btn.innerText;            
+            console.log(calculator.firstOperand);
+            if (calculator.firstOperand === null) {
+                calculator.firstOperand = calculator.currentOperand;
+                calculator.currentOperand = "0";
+                calculator.currentOperator = btn.textContent;
+            }            
+            else {
+                if (calculator.currentOperator === "+") {
+                    temp = calculator.result;
+                    calculator.result = parseInt(calculator.firstOperand) + parseInt(calculator.currentOperand);
+                    result.innerText = calculator.result;
+                    calculator.currentOperator = btn.innerText;
+                    calculator.firstOperand = calculator.result;
+                    calculator.currentOperand = 0;
+                }
+                
+                else if (calculator.currentOperator === "-") {
+                    temp = calculator.result;
+                    calculator.result = parseInt(calculator.firstOperand) - parseInt(calculator.currentOperand);
+                    result.innerText = calculator.result;
+                    calculator.currentOperator = btn.textContent;
+                    calculator.firstOperand = calculator.result;
+                    calculator.currentOperand = 0;
+                }
+                else if (calculator.currentOperator === "x") {
+                    temp = calculator.result;
+                    calculator.result = parseInt(calculator.firstOperand) * parseInt(calculator.currentOperand);
+                    result.innerText = calculator.result;
+                    calculator.currentOperator = btn.textContent;
+                    calculator.firstOperand = calculator.result;
+                    calculator.currentOperand = 0;
+                }
+                else if (calculator.currentOperator === "/") {
+                    temp = calculator.result;
+                    calculator.result = parseInt(calculator.firstOperand) / parseInt(calculator.currentOperand);
+                    result.innerText = calculator.result;
+                    calculator.currentOperator = btn.textContent;
+                    calculator.firstOperand = calculator.result;
+                    calculator.currentOperand = 0;
+                }
+            }          
         }
         else {
-            input.innerText = input.innerText + btn.innerText;
-            calculator.firstOperand = `${calculator.firstOperand} + ${calculator.currentOperand}`;
-            calculator.currentOperand = input.innerText;
-    }})
+            input.innerText += btn.innerText;
+            if (btn.innerText === "Ans") {
+                calculator.currentOperand = previousAnswer;
+            }
+            else {
+                calculator.currentOperand += btn.innerText;
+            }
+        }})
+})
+
+answer.addEventListener('click', function() {
+    if (input.innerText === "Enter a number.") {
+        input.innerText = "Ans";
+    }
+    else {
+        input.innerText += "Ans";
+    }
+})
+
+undo.addEventListener('click', function() {
+    input.innerText = input.innerText.slice(0, -1);
+    calculator.currentOperand = calculator.currentOperand.slice(0, -1);
 })
 
 clear.addEventListener('click', function() {
-    input.innerText = "0";
+    input.innerText = "Enter a number.";
     isBracket = false;
+    previousAnswer = calculator.result;
+    result.innerText = 0;
+    calculator.result = 0;
+    calculator.currentOperand = null;
+    calculator.firstOperand = null;
+    calculator.currentOperator = null;
 })
 
 negative.addEventListener('click', function() {
@@ -53,8 +122,32 @@ negative.addEventListener('click', function() {
 })
 
 equal.addEventListener('click', function() {
-    calculate(input);
-
+    if (calculator.currentOperator === "+") {
+        calculator.result = parseInt(calculator.firstOperand) + parseInt(calculator.currentOperand);
+        result.innerText = calculator.result;
+        calculator.firstOperand = calculator.result;
+        calculator.currentOperand = 0;
+    }
+    
+    else if (calculator.currentOperator === "-") {
+        calculator.result = parseInt(calculator.firstOperand) - parseInt(calculator.currentOperand);
+        result.innerText = calculator.result;
+        calculator.firstOperand = calculator.result;
+        calculator.currentOperand = 0;
+    }
+    else if (calculator.currentOperator === "x") {
+        calculator.result = parseInt(calculator.firstOperand) * parseInt(calculator.currentOperand);
+        result.innerText = calculator.result;
+        calculator.firstOperand = calculator.result;
+        calculator.currentOperand = 0;
+    }
+    else if (calculator.currentOperator === "/") {
+        calculator.result = parseInt(calculator.firstOperand) / parseInt(calculator.currentOperand);
+        result.innerText = calculator.result;
+        calculator.firstOperand = calculator.result;
+        calculator.currentOperand = 0;
+    }
+    input.innerText = "Enter a number."
 });
 
 bracket.addEventListener('click', function() {
@@ -70,8 +163,3 @@ bracket.addEventListener('click', function() {
         isBracket = false;
     }
 });
-
-function calculate(input) {
-    let sum = input.innerText;
-    let arrNum
-}
