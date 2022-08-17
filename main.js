b1df8e70ef998cf6ac6c1fd50c4118d211b73d02
Jamie Furlong <jamiefurlong16@gmail.com>
2022-08-17 14:53:35 +0100

    All functionality added.
    Functionality added for percentage, power, negative and decimals.
    Further bug checks needed for power and percentage in particular.
    
    Bracket functionality removed and to be pushed to new branch.

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ const arrBtn = Array.from(buttons);
 const clear = document.querySelector('.clear'); // C button.
 const negative = document.querySelector('.negative'); // negation button
 const equal = document.querySelector('.equal'); // equal button
-const bracket = document.querySelector('.bracket'); // bracket button
 const answer = document.querySelector('.ans');
 const undo = document.querySelector('.undo');
-let isBracket = false; // bool to determine the existence of an opening bracket.
+const decimal = document.querySelector('.decimal');
+const percentage = document.querySelector('.percentage');
+let hasDecimal = false; // bool to determine if an operand contains a decimal.
 let previousAnswer = NaN;
 let temp;
 
@@ -27,6 +28,9 @@ const calculator = {
 arrBtn.forEach(btn => {
     btn.addEventListener('click', function() {
         if (input.innerText === "Enter a number.") {
+            input.style.fontSize = '2rem';
+            result.style.fontSize = '1rem';
+            result.innerText = 0;
                 if (btn.innerText < "0" || btn.innerText > "9") {
                     return input.innerText = "ERROR";
                 }
@@ -34,9 +38,24 @@ arrBtn.forEach(btn => {
             calculator.currentOperand = btn.innerText;
             console.log(`currentOperand is: ${calculator.currentOperand}`);
         }
-        else if (btn.innerText === "+" || btn.innerText === "-" || btn.innerText === "x" || btn.innerText === "/") {
-            input.innerText += btn.innerText;            
-            console.log(calculator.firstOperand);
+
+        else if (input.innerText === "ERROR") {
+            if (btn.innerText === "+" || btn.innerText === "-" || btn.innerText === "x" || btn.innerText === "/" || btn.innerText === "^") {
+                input.innerText = "ERROR";
+            }
+        }
+        
+        else if (btn.innerText === "+" || btn.innerText === "-" || btn.innerText === "x" || btn.innerText === "/" || btn.innerText === "^") {
+            
+            input.innerText += btn.innerText;  
+            let string3 = calculator.currentOperand;
+            string3 = string3.toString();
+            if (string3.indexOf("%") > 0) {
+                string3 = string3.slice(0, -1);
+                calculator.currentOperand = string3;
+                calculator.currentOperand = ((parseFloat(calculator.firstOperand) / 100) * parseFloat(calculator.currentOperand));
+            }          
+
             if (calculator.firstOperand === null) {
                 calculator.firstOperand = calculator.currentOperand;
                 calculator.currentOperand = "0";
@@ -45,7 +64,20 @@ arrBtn.forEach(btn => {
             else {
                 if (calculator.currentOperator === "+") {
                     temp = calculator.result;
-                    calculator.result = parseInt(calculator.firstOperand) + parseInt(calculator.currentOperand);
+                    if (calculator.firstOperand === "Ans") {
+                        calculator.firstOperand = previousAnswer;
+                    }
+                    let string1 = calculator.firstOperand;
+                    string1 = string1.toString();
+                    let string2 = calculator.currentOperand;
+                    string2 = string2.toString();
+                    
+                    if (string1.indexOf('.') > 0 || string2.indexOf('.') > 0) {
+                        calculator.result = parseFloat(calculator.firstOperand) + parseFloat(calculator.currentOperand);
+                    }
+                    else {
+                        calculator.result = parseInt(calculator.firstOperand) + parseInt(calculator.currentOperand);
+                    }
                     result.innerText = calculator.result;
                     calculator.currentOperator = btn.innerText;
                     calculator.firstOperand = calculator.result;
@@ -54,7 +86,20 @@ arrBtn.forEach(btn => {
                 
                 else if (calculator.currentOperator === "-") {
                     temp = calculator.result;
-                    calculator.result = parseInt(calculator.firstOperand) - parseInt(calculator.currentOperand);
+                    if (calculator.firstOperand === "Ans") {
+                        calculator.firstOperand = previousAnswer;
+                    }
+                    let string1 = calculator.firstOperand;
+                    string1 = string1.toString();
+                    let string2 = calculator.currentOperand;
+                    string2 = string2.toString();
+                    
+                    if (string1.indexOf('.') > 0 || string2.indexOf('.') > 0) {
+                        calculator.result = parseFloat(calculator.firstOperand) - parseFloat(calculator.currentOperand);
+                    }
+                    else {
+                        calculator.result = parseInt(calculator.firstOperand) - parseInt(calculator.currentOperand);
+                    }
                     result.innerText = calculator.result;
                     calculator.currentOperator = btn.textContent;
                     calculator.firstOperand = calculator.result;
@@ -62,7 +107,20 @@ arrBtn.forEach(btn => {
                 }
                 else if (calculator.currentOperator === "x") {
                     temp = calculator.result;
-                    calculator.result = parseInt(calculator.firstOperand) * parseInt(calculator.currentOperand);
+                    if (calculator.firstOperand === "Ans") {
+                        calculator.firstOperand = previousAnswer;
+                    }
+                    let string1 = calculator.firstOperand;
+                    string1 = string1.toString();
+                    let string2 = calculator.currentOperand;
+                    string2 = string2.toString();
+                    
+                    if (string1.indexOf('.') > 0 || string2.indexOf('.') > 0) {
+                        calculator.result = parseFloat(calculator.firstOperand) * parseFloat(calculator.currentOperand);
+                    }
+                    else {
+                        calculator.result = parseInt(calculator.firstOperand) * parseInt(calculator.currentOperand);
+                    }
                     result.innerText = calculator.result;
                     calculator.currentOperator = btn.textContent;
                     calculator.firstOperand = calculator.result;
@@ -70,11 +128,50 @@ arrBtn.forEach(btn => {
                 }
                 else if (calculator.currentOperator === "/") {
                     temp = calculator.result;
-                    calculator.result = parseInt(calculator.firstOperand) / parseInt(calculator.currentOperand);
+                    if (calculator.firstOperand === "Ans") {
+                        calculator.firstOperand = previousAnswer;
+                    }
+                    let string1 = calculator.firstOperand;
+                    string1 = string1.toString();
+                    let string2 = calculator.currentOperand;
+                    string2 = string2.toString();
+                    
+                    if (string1.indexOf('.') > 0 || string2.indexOf('.') > 0) {
+                        calculator.result = parseFloat(calculator.firstOperand) / parseFloat(calculator.currentOperand);
+                    }
+                    else {
+                        calculator.result = parseInt(calculator.firstOperand) / parseInt(calculator.currentOperand);
+                    }
                     result.innerText = calculator.result;
                     calculator.currentOperator = btn.textContent;
                     calculator.firstOperand = calculator.result;
                     calculator.currentOperand = 0;
+                }
+                else if (calculator.currentOperator === "^") {
+                    temp = calculator.result;
+                    if (calculator.firstOperand === "Ans") {
+                        calculator.firstOperand = previousAnswer;
+                    }
+                    let string1 = calculator.firstOperand;
+                    string1 = string1.toString();
+                    let string2 = calculator.currentOperand;
+                    string2 = string2.toString();
+                    
+
+                    if (string1.indexOf('.') > 0 || string2.indexOf('.') > 0) {
+                        calculator.result = Math.pow(parseFloat(calculator.firstOperand), parseFloat(calculator.currentOperand));                        
+                    }
+                    else {
+                        calculator.result = Math.pow(parseInt(calculator.firstOperand), parseInt(calculator.currentOperand));
+                    }
+                        result.innerText = calculator.result;
+                        calculator.currentOperator = btn.textContent;
+                        calculator.currentOperand = 0;
+                        calculator.firstOperand = null;
+                        input.style.fontSize = '1rem';
+                        result.style.fontSize = '2rem';
+                        input.innerText = "Enter a number."
+                        previousAnswer = calculator.result;
                 }
             }          
         }
@@ -89,12 +186,29 @@ arrBtn.forEach(btn => {
         }})
 })
 
+decimal.addEventListener('click', function() {
+    if (input.innerText === "Enter a number.") {
+        calculator.currentOperand = "0.";
+        input.innerText = "0."
+    }
+    else {
+        calculator.currentOperand += ".";
+        input.innerText += ".";
+    }
+})
+
 answer.addEventListener('click', function() {
     if (input.innerText === "Enter a number.") {
         input.innerText = "Ans";
+        calculator.currentOperand = previousAnswer;
+        input.style.fontSize = '2rem';
+        result.style.fontSize = '1rem';
     }
     else {
         input.innerText += "Ans";
+        calculator.currentOperand = previousAnswer;
+        input.style.fontSize = '2rem';
+        result.style.fontSize = '1rem';
     }
 })
 
@@ -105,61 +219,160 @@ undo.addEventListener('click', function() {
 
 clear.addEventListener('click', function() {
     input.innerText = "Enter a number.";
-    isBracket = false;
     previousAnswer = calculator.result;
     result.innerText = 0;
     calculator.result = 0;
     calculator.currentOperand = null;
     calculator.firstOperand = null;
-    calculator.currentOperator = null;
+    calculator.currentOperator = 0;
 })
 
 negative.addEventListener('click', function() {
-    if (input.innerText === "0") {
-        input.innerText = "";
+    if (input.innerText === "Enter a number.") {
+        input.innerText = "(-";
+        calculator.currentOperand = "-";
     }
-    input.innerText = input.innerText + "(-";
+    else {
+        input.innerText = input.innerText + "(-";
+        calculator.currentOperand = "-";
+    }
 })
 
+percentage.addEventListener('click', function() {
+    if (input.innerText === "Enter a number.") {
+        input.innerText = "ERROR";
+    }
+    else {
+        let string1 = calculator.currentOperand;
+        string1 = string1.toString();
+        if (string1[0] === "%") {
+            input.innerText = "ERROR";
+        }
+        else {
+            input.innerText += "%";
+            calculator.currentOperand += "%";            
+        }
+}});
+
 equal.addEventListener('click', function() {
+    let string3 = calculator.currentOperand;
+    string3 = string3.toString();
+    if (string3.indexOf("%") > 0) {
+        string3 = string3.slice(0, -1);
+        calculator.currentOperand = string3;
+        calculator.currentOperand = ((parseFloat(calculator.firstOperand) / 100) * parseFloat(calculator.currentOperand));
+    }
     if (calculator.currentOperator === "+") {
-        calculator.result = parseInt(calculator.firstOperand) + parseInt(calculator.currentOperand);
+        if (calculator.firstOperand === "Ans") {
+            calculator.firstOperand = previousAnswer;
+        }
+        let string1 = calculator.firstOperand;
+        string1 = string1.toString();
+        let string2 = calculator.currentOperand;
+        string2 = string2.toString();
+        
+        if (string1.indexOf('.') > 0 || string2.indexOf('.') > 0) {
+            calculator.result = parseFloat(calculator.firstOperand) + parseFloat(calculator.currentOperand);
+        }
+        else {
+            calculator.result = parseInt(calculator.firstOperand) + parseInt(calculator.currentOperand);
+        }
         result.innerText = calculator.result;
-        calculator.firstOperand = calculator.result;
+        calculator.firstOperand = null;
         calculator.currentOperand = 0;
+        input.style.fontSize = '1rem';
+        result.style.fontSize = '2rem';
     }
     
     else if (calculator.currentOperator === "-") {
-        calculator.result = parseInt(calculator.firstOperand) - parseInt(calculator.currentOperand);
+        if (calculator.firstOperand === "Ans") {
+            calculator.firstOperand = previousAnswer;
+        }
+        let string1 = calculator.firstOperand;
+        string1 = string1.toString();
+        let string2 = calculator.currentOperand;
+        string2 = string2.toString();
+        
+        if (string1.indexOf('.') > 0 || string2.indexOf('.') > 0) {
+            calculator.result = parseFloat(calculator.firstOperand) - parseFloat(calculator.currentOperand);
+        }
+        else {
+            calculator.result = parseInt(calculator.firstOperand) - parseInt(calculator.currentOperand);
+        }
         result.innerText = calculator.result;
-        calculator.firstOperand = calculator.result;
+        calculator.firstOperand = null;
         calculator.currentOperand = 0;
+        input.style.fontSize = '1rem';
+        result.style.fontSize = '2rem';
     }
     else if (calculator.currentOperator === "x") {
-        calculator.result = parseInt(calculator.firstOperand) * parseInt(calculator.currentOperand);
+        if (calculator.firstOperand === "Ans") {
+            calculator.firstOperand = previousAnswer;
+        }
+        let string1 = calculator.firstOperand;
+        string1 = string1.toString();
+        let string2 = calculator.currentOperand;
+        string2 = string2.toString();
+        
+        if (string1.indexOf('.') > 0 || string2.indexOf('.') > 0) {
+            calculator.result = parseFloat(calculator.firstOperand) * parseFloat(calculator.currentOperand);
+        }
+        else {
+            calculator.result = parseInt(calculator.firstOperand) * parseInt(calculator.currentOperand);
+        }
         result.innerText = calculator.result;
-        calculator.firstOperand = calculator.result;
+        calculator.firstOperand = null;
         calculator.currentOperand = 0;
+        input.style.fontSize = '1rem';
+        result.style.fontSize = '2rem';
     }
     else if (calculator.currentOperator === "/") {
-        calculator.result = parseInt(calculator.firstOperand) / parseInt(calculator.currentOperand);
+        if (calculator.firstOperand === "Ans") {
+            calculator.firstOperand = previousAnswer;
+        }
+        let string1 = calculator.firstOperand;
+        string1 = string1.toString();
+        let string2 = calculator.currentOperand;
+        string2 = string2.toString();
+        
+        if (string1.indexOf('.') > 0 || string2.indexOf('.') > 0) {
+            calculator.result = parseFloat(calculator.firstOperand) / parseFloat(calculator.currentOperand);
+        }
+        else {
+            calculator.result = parseInt(calculator.firstOperand) / parseInt(calculator.currentOperand);
+        }
         result.innerText = calculator.result;
-        calculator.firstOperand = calculator.result;
+        calculator.firstOperand = null;
         calculator.currentOperand = 0;
+        input.style.fontSize = '1rem';
+        result.style.fontSize = '2rem';
+    }
+    else if (calculator.currentOperator === "^") {
+        temp = calculator.result;
+        if (calculator.firstOperand === "Ans") {
+            calculator.firstOperand = previousAnswer;
+        }
+        let string1 = calculator.firstOperand;
+        string1 = string1.toString();
+        let string2 = calculator.currentOperand;
+        string2 = string2.toString();
+        let copyFirst = calculator.firstOperand;
+
+        if (string1.indexOf('.') > 0 || string2.indexOf('.') > 0) {
+            calculator.result = Math.pow(parseFloat(calculator.firstOperand), parseFloat(calculator.currentOperand));                        
+        }
+        else {
+            calculator.result = Math.pow(parseInt(calculator.firstOperand), parseInt(calculator.currentOperand));
+        }
+            result.innerText = calculator.result;
+            calculator.firstOperand = null;
+            calculator.currentOperand = 0;
+            input.style.fontSize = '1rem';
+            result.style.fontSize = '2rem';
     }
     input.innerText = "Enter a number."
+    previousAnswer = calculator.result;
 });
 
-bracket.addEventListener('click', function() {
-    if (isBracket === false) {
-        if (input.innerText === "0") {
-            input.innerText = "";
-            input.innerText = input.innerText + "(";
-            isBracket = true;
-        }
-    }
-    else {
-        input.innerText = input.innerText + ")";
-        isBracket = false;
-    }
-});
+
+
